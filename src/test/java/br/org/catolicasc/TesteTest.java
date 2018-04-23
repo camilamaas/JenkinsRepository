4def7c0020a2019c67835840881a63229e7355b5
Camila <Camila@DESKTOP-CB91C1B>
2018-04-23 19:54:37 -0300

    Teste Inicial

---
+++ src/test/java/br/org/catolicasc/TesteTest.java
@@ -0,0 +1,43 @@
+package br.org.catolicasc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TesteTest {
+
+	@Test
+	public void teste1() {
+		assertTrue(true);
+	}
+	
+	@Test 
+	public void somaSucesso() {
+		assertEquals(2, soma(1,1));
+	}
+	
+	@Test 
+	public void somaErro() {
+		assertNotEquals(1, soma(1,1));
+	}
+	
+	@Test 
+	public void diminuiSucesso() {
+		assertEquals(2, diminui(5,3));
+	}
+	
+	@Test 
+	public void diminuiErro() {
+		assertNotEquals(1, diminui(1,1));
+	}
+	
+	public int soma(int valor1, int valor2 ) {
+		return valor1+valor2; 
+	}
+	
+	public int diminui(int valor1, int valor2 ) {
+		return valor1-valor2; 
+	}
+}
